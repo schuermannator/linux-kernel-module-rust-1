de8323a1a34c05d73ee525df61fcb75d706e570c
Ultimatespirit <ultimatespirit@users.noreply.github.com>
2020-04-18 04:48:28 -0400

    Preliminary test of implementing sync
    Sync implementation from lizhu copied into this repository and
    appropriate modifications to hopefully run it are made.

--- src/bindings_helper.h
+++ src/bindings_helper.h
@@ -6,6 +6,12 @@
 #include <linux/uaccess.h>
 #include <linux/version.h>
 
+// Custom additions for locks
+#include <linux/spinlock.h>
+#include <linux/spinlock_types.h>
+#include <linux/mutex.h>
+
+
 // Bindgen gets confused at certain things
 //
 const gfp_t BINDINGS_GFP_KERNEL = GFP_KERNEL;
